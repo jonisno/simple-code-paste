6cb47b7827f58587267125752c6b3d8a2f11209b
jonisno <jonis@jonis.no>
2014-10-27 10:58:38 +0000

    Gitignore, CSRF tokens, config file and check for existing token.
    Moved some stuff out in config files, added CSRF token check.
    Added sample.conf.
    	Now runs a check to see if token already exists.

--- paste.pl
+++ paste.pl
@@ -1,26 +1,29 @@
 #/usr/bin/perl
 use Mojolicious::Lite;
+use Math::Base::Convert qw/cnv/;
 use Mojo::Pg;
 use 5.20.1;
 use experimental 'signatures';
 
-app->config({
-	hypnotoad => {
-		listen  => ['http://localhost:3000'],
-		workers => 10,
-		proxy   => 1
-	}
-});
+plugin 'Config';
 
-helper pg => sub { state $pg = Mojo::Pg->new('postgresql://user:pass@localhost/storage') };
+app->secrets(app->config('secrets'));
+
+helper pg => sub { state $pg = Mojo::Pg->new(app->config('dsn')) };
 
 app->pg->db->do(
 	'create table if not exists paste(
-		id uuid default uuid_generate_v4(),
+		id serial,
+		token text unique,
 		content text,
 		primary key(id)
 	)'
 );
+
+helper generate_token => sub {
+	my $id = shift;
+	return cnv($id + int rand(9999999), 'dec', 'b62');
+};
 
 get '/' => sub ($c) {
 	$c->render('index');
@@ -33,15 +36,30 @@ get '/p' => sub ($c) {
 post '/p/save' => sub ($c) {
 	my $db = $c->pg->db;
 	my $paste = $c->param('paste');
+
+	# CSRF protection.
+	my $val = $c->validation;
+	return $c->render(text => 'Invalid CSRF token', status => 403) if $val->csrf_protect->has_error('csrf_token');
+
 	my $res = $db->query('insert into paste(content) values (?) returning id' => $paste);
-	$c->redirect_to("/p/" . $res->hash->{id});
+	my $id = $res->hash->{id};
+
+	# Generate token and make sure it's not already in the database.
+	my $token = $c->generate_token($id);
+	my $tokencheck = $db->query('select token from paste where token = ?' => $token)->hash;
+	while($tokencheck) {
+		$token = $c->generate_token($id);
+		$tokencheck = $db->query('select token from paste where token = ?' => $token)->hash;
+	};
+
+	$db->query('update paste set token = ? where id = ?' => ($token, $id));
+	$c->redirect_to("/p/$token");
 } => 'pastesave';
 
-get '/p/:id' => sub ($c) {
+get '/p/:token' => sub ($c) {
 	my $db = $c->pg->db;
-	my $id = $c->param('id');
-	return $c->reply->not_found unless $id =~ /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
-	my $res = $db->query('select content from paste where id = (?)' => $id)->hash;
+	my $token = $c->param('token');
+	my $res = $db->query('select content from paste where token = (?)' => $token)->hash;
 	return $c->reply->not_found unless $res;
 	$c->stash(content => $res->{content});
 	$c->render('paste/show');
